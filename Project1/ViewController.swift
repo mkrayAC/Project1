bc3602f2970901c70558f2f4c41e8a1e22cad8f3
mkrai <mkray3.0@hotmail.com>
2016-11-29 17:14:20 -0600

    Storm viewer complete

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pictures = [String]()
 
+    //stard viewDidload
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        title = "Storm viewer"
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try!fm.contentsOfDirectory(atPath: path)
+        
+        for items in items {
+            if items.hasPrefix("nssl"){
+            pictures.append(items)
+                
+            }
+        }
         // Do any additional setup after loading the view, typically from a nib.
+        print(pictures)
+        
     }
+    // end viewDidload
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //start numberofRows function
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return  pictures.count
     }
+    //end numberofrows function
 
-
+    //start cellforrow function
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    //end cellforrow function
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
